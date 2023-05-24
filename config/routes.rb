678397e92ff3d6db0cb130827fdf2f34f35942ea
Bazik1677 <125483625+Bazik1677@users.noreply.github.com>
2023-05-24 15:53:53 +0200

    exo 2 with refacto

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'restaurants/index'
-  get 'restaurants/show'
-  get 'restaurants/new'
-  get 'restaurants/create'
+  # get 'reviews/new'
+  # get 'reviews/create'
+  # get 'restaurants/index'
+  # get 'restaurants/show'
+  # get 'restaurants/new'
+  # get 'restaurants/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants, only: %i[index show new create] do
-    resources :reviews, only: %i[new create]
+    resources :reviews, only: %i[create]
   end
 end
