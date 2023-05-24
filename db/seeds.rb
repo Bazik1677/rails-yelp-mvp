fa421197974da2872af2640ed758deac3af86f35
Bazik1677 <125483625+Bazik1677@users.noreply.github.com>
2023-05-24 15:40:42 +0200

    exo 2 sans refacto

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese"}
+Elementaire = {name: "Elementaire", address: "30 rue de lapix", category: "french"}
+Plume = {name: "Plume", address: "5bis rue du louvre", category: "japanese"}
+Mamapapa = {name: "mamapapa", address: "105 avenue des brigands", category: "italian"}
+La_frite =  {name: "La_frite", address: "56A Shoreditch High St, London E1 6PQ", category: "belgian"}
+
+[Elementaire, Plume, Mamapapa, dishoom, La_frite].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
+
